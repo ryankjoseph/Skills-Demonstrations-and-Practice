6625b9ce37acd490f6f362618027cb80501539cd
Ryan Joseph <ryankjoseph@gmail.com>
2021-05-14 01:10:57 -0400

    Adding starter code for testing promises

---
+++ 5_2_Javascript-Promises/mongoose-movies/seeds.js
@@ -0,0 +1,5 @@
+ require('./config/database');// connect this script to the database
+ const Movie = require('./models/movie');
+ const Performer = require('./models/performer');
+ const data = require('./data');
+ 
