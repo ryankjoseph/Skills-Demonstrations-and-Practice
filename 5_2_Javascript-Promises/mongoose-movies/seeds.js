6f04ea4cd23e27b1cbb895d15e9a1509b6d4dcb3
Ryan Joseph <ryankjoseph@gmail.com>
2021-05-14 01:49:30 -0400

    Further asynchronous add function testing, and database manipulation using .then

--- 5_2_Javascript-Promises/mongoose-movies/seeds.js
+++ 5_2_Javascript-Promises/mongoose-movies/seeds.js
@@ -11,13 +11,68 @@
 //    console.log(result)
 //  })
 
-const p = new Promise(function(resolve,reject){
-  setTimeout(()=>{
-    reject('something went wrong');
-  },2000)
-} );
-p.then(function(result){
-  console.log(result)
-}).catch( (err)=> {
-  console.log(err)
+// const p = new Promise(function(resolve,reject){
+//   setTimeout(()=>{
+//     value = 55;
+//     if(value) resolve(value)
+//     reject('something went wrong');
+//   },2000)
+// } );
+// p.then(function(result){
+//   console.log(result)
+// }).catch( (err)=> {
+//   console.log(err)
+// })
+// const p = new Promise(function(resolve,reject){
+//   setTimeout(()=>{
+//     value = 55;
+//     if(value) resolve(value)
+//     reject('something went wrong');
+//   },2000)
+// } );
+// p.then(function(result){
+//   console.log(result)
+//   return 42
+// }).catch( (err)=> {
+//   console.log(err)
+// })
+// .then(function(result){
+//   console.log(result)
+//   return "Done!"
+// })
+// .then(function(result){
+//   console.log(result)
+// })
+
+// function asyncAdd(a,b, delay){
+//   return new Promise(function(resolve){
+//     setTimeout(()=>{
+//       resolve(a+b);
+//     }, delay);
+//   });
+// }
+
+// asyncAdd(5,10,2000)
+// .then(function(sum){
+//   console.log(sum);
+//   return asyncAdd(sum, 100, 1000);
+// })
+// .then(function(sum){
+//   console.log(sum)
+//   return asyncAdd(sum, 1000,2000)
+// })
+// .then(function(sum){
+//   console.log(sum);
+// });
+
+Movie.deleteMany({})
+.then(function(results){
+  console.log('Deleted Movies:', results);
+  return Performer.deleteMany({});
+})
+.then(function(results){
+  console.log("Deleted Performers:", results);
+})
+.then(function(){
+  process.exit();
 })
