9713de07de9d7d59fa59dd39c9e1c5263e283ecd
Ryan Joseph <ryankjoseph@gmail.com>
2021-05-14 01:22:25 -0400

    First asynchronous promise rejection

--- 5_2_Javascript-Promises/mongoose-movies/seeds.js
+++ 5_2_Javascript-Promises/mongoose-movies/seeds.js
@@ -2,11 +2,20 @@
  const Movie = require('./models/movie');
  const Performer = require('./models/performer');
  const data = require('./data');
- const p = new Promise(function(resolve,reject){
-   setTimeout(()=>{
-     resolve(55);
-   },2000)
- } );
- p.then(function(result){
-   console.log(result)
- })
+//  const p = new Promise(function(resolve,reject){
+//    setTimeout(()=>{
+//      resolve(55);
+//    },2000)
+//  } );
+//  p.then(function(result){
+//    console.log(result)
+//  })
+
+const p = new Promise(function(resolve,reject){
+  setTimeout(()=>{
+    reject('something went wrong');
+  },2000)
+} );
+p.then(function(result){
+  console.log(result)
+})
